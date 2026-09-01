7a6686e29124e37205c126bc0e6dedc8d4e848c3
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-02 00:41:24 +0530

    js and nodejs code

---
+++ Javascript/54_intro2.js
@@ -0,0 +1,11 @@
+/* it can run in terminal   node new.js */
+
+console.log("Hello World")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+/* error show in another intro file */
+
+/* js only for browser but notejs help us to run in terminal */
+/* browser mai jyda feature melta hai */
